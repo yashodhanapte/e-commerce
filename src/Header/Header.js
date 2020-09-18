0fb54adc08df92efdc4977b5ab37fc57484c2321
Yashodhan <yashodhan.apte@synerzip.com>
2020-09-19 02:29:09 +0530

    added different shoppingcart image

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import {Link} from 'react-router-dom';
 import {useStateValue} from '../StateProvider';
 
@@ -46,7 +47,7 @@ const Header = () => {
                 </div>
                 <Link to="/checkout">
                     <div className="header__optionBasket">
-                        <ShoppingBasketIcon />
+                        <ShoppingCartIcon />
                         <span className="header__optionLineTwo header__basketCount">
                             {basket?.length}
                         </span>
